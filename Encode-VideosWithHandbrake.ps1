68a2585a5b3a83938fc69562927ddfa3ad44e662
Chris Bensch <chris.bensch@gmail.com>
2023-10-14 20:24:20 -0700

    File renaming for later delete (Safety)

--- Encode-VideosWithHandbrake.ps1
+++ Encode-VideosWithHandbrake.ps1
@@ -1,6 +1,6 @@
-﻿
+﻿# 
 
-$filelist = Get-ChildItem C:\Temp\MoviesTemp\ -recurse
+$filelist = Get-ChildItem C:\Temp\MoviesTemp\ -Recurse -File | Where-Object {$_.Extension -eq '.mkv' -or $_.Extension -eq '.mp4'}
  
 $num = $filelist | Measure-Object
 $filecount = $num.count
@@ -9,8 +9,14 @@ $i = 0;
 ForEach ($file in $filelist)
 {
     $i++;
-    $originalFileName = $file.DirectoryName + "\" + $file.BaseName + $file.Extension;
-    $newfile = $file.DirectoryName + "\" + $file.BaseName + ".mp4";
+    $oldfile = $file.DirectoryName + "\" + $file.BaseName + $file.Extension
+    $tempfile = $file.DirectoryName + "\" + "old_" + $file.BaseName + $file.Extension
+    #$oldFileName = $originalFileName + "_old"
+    #Rename-Item -Path $originalFileName -NewName $oldFileName
+    Rename-Item -Path $oldfiel -NewName $tempfile
+
+    
+    $newfile = $file.DirectoryName + "\" + $file.BaseName + ".mp4"
       
     $progress = ($i / $filecount) * 100
     $progress = [Math]::Round($progress,2)
@@ -18,10 +24,10 @@ ForEach ($file in $filelist)
     Clear-Host
     Write-Host -------------------------------------------------------------------------------
     Write-Host Handbrake Batch Encoding
-    Write-Host "Processing - $originalFileName"
+    Write-Host "Processing - $oldfile"
     Write-Host "File $i of $filecount - $progress%"
     Write-Host -------------------------------------------------------------------------------
      
-    #Start-Process "C:\Program Files\HandBrake\HandBrakeCLI.exe" -ArgumentList "-i `"$originalFileName`" -t 1 --angle 1 -c 1 -o `"$newfile`" -f mp4  -O  --decomb --modulus 16 -e x264 -q 32 --vfr -a 1 -E lame -6 dpl2 -R Auto -B 48 -D 0 --gain 0 --audio-fallback ffac3 --x264-preset=veryslow  --x264-profile=high  --x264-tune=`"animation`"  --h264-level=`"4.1`"  --verbose=0" -Wait -NoNewWindow
-    Start-Process "C:\Tools\HandBrakeCLI\HandBrakeCLI.exe" -ArgumentList "-i `"$originalFileName`" -o `"$newfile`" --preset-import-file `"C:\Tools\Tdarr\Bensch4k-HW-Handbrake.json`" -Z `"Bensch4kHW`" " -Wait -NoNewWindow
+    #Start-Process "C:\Program Files\HandBrake\HandBrakeCLI.exe" -ArgumentList "-i `"$oldfile`" -t 1 --angle 1 -c 1 -o `"$newfile`" -f mp4  -O  --decomb --modulus 16 -e x264 -q 32 --vfr -a 1 -E lame -6 dpl2 -R Auto -B 48 -D 0 --gain 0 --audio-fallback ffac3 --x264-preset=veryslow  --x264-profile=high  --x264-tune=`"animation`"  --h264-level=`"4.1`"  --verbose=0" -Wait -NoNewWindow
+    Start-Process "C:\Tools\HandBrakeCLI\HandBrakeCLI.exe" -ArgumentList "-i `"$oldfile`" -o `"$newfile`" --preset-import-file `"C:\Tools\Tdarr\Bensch4k-HW-Handbrake.json`" -Z `"Bensch4kHW`" " -Wait -NoNewWindow
 }
